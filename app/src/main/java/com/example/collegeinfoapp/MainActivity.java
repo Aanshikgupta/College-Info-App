380d8b5f1cc3cf831e2919c93dba720438f110f7
Aanshikgupta <gupta.aanshik@gmail.com>
2021-05-19 05:06:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/collegeinfoapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.collegeinfoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
